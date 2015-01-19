7168018dda6f4b6ea7d8ede5df5ff6e0f6b9f3f3
Harry Xiao <shawhu@gmail.com>
2015-01-19 22:16:25 +0800

    History diagrams

--- BD-Dashboard/BD-Dashboard/frmHistory.Designer.cs
+++ BD-Dashboard/BD-Dashboard/frmHistory.Designer.cs
@@ -34,6 +34,10 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.chkAutoRefresh = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.chkShowDiagram = new System.Windows.Forms.CheckBox();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,18 +47,21 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 80);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(560, 497);
+            this.dataGridView1.Size = new System.Drawing.Size(1142, 746);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("SimHei", 15.75F);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 21);
+            this.label1.Size = new System.Drawing.Size(143, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "历史记录";
             // 
@@ -65,38 +72,92 @@
             // 
             // chkAutoRefresh
             // 
+            this.chkAutoRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.chkAutoRefresh.AutoSize = true;
-            this.chkAutoRefresh.Location = new System.Drawing.Point(492, 15);
+            this.chkAutoRefresh.Location = new System.Drawing.Point(1056, 23);
+            this.chkAutoRefresh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.chkAutoRefresh.Name = "chkAutoRefresh";
-            this.chkAutoRefresh.Size = new System.Drawing.Size(74, 17);
+            this.chkAutoRefresh.Size = new System.Drawing.Size(99, 24);
             this.chkAutoRefresh.TabIndex = 2;
             this.chkAutoRefresh.Text = "自动更新";
             this.chkAutoRefresh.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(386, 11);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(905, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(126, 35);
             this.button1.TabIndex = 3;
             this.button1.Text = "  清除历史数据";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chkShowDiagram
+            // 
+            this.chkShowDiagram.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkShowDiagram.AutoSize = true;
+            this.chkShowDiagram.Checked = true;
+            this.chkShowDiagram.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkShowDiagram.Location = new System.Drawing.Point(799, 17);
+            this.chkShowDiagram.Name = "chkShowDiagram";
+            this.chkShowDiagram.Size = new System.Drawing.Size(83, 30);
+            this.chkShowDiagram.TabIndex = 4;
+            this.chkShowDiagram.Text = "显示表格";
+            this.chkShowDiagram.UseVisualStyleBackColor = true;
+            this.chkShowDiagram.CheckedChanged += new System.EventHandler(this.chkShowDiagram_CheckedChanged);
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(215, 22);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(145, 26);
+            this.dtpStart.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(470, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "检索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "日期";
+            // 
             // frmHistory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.ClientSize = new System.Drawing.Size(1178, 844);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.chkShowDiagram);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chkAutoRefresh);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmHistory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "上海白朵食用菌有限公司-历史记录";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmHistory_FormClosing);
             this.Load += new System.EventHandler(this.frmHistory_Load);
+            this.ResizeBegin += new System.EventHandler(this.frmHistory_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.frmHistory_ResizeEnd);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmHistory_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,5 +172,9 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.CheckBox chkAutoRefresh;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkShowDiagram;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- BD-Dashboard/BD-Dashboard/frmHistory.cs
+++ BD-Dashboard/BD-Dashboard/frmHistory.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+
 namespace BD_Dashboard
 {
     public partial class frmHistory : Form
@@ -23,30 +24,53 @@ namespace BD_Dashboard
             InitializeComponent();
         }
 
+        //private DateTime earliest_date = DateTime.Parse("2014-11-1");
+        private DateTime earliest_date = DateTime.Parse(DateTime.Now.ToShortDateString());
         private void frmHistory_Load(object sender, EventArgs e)
         {
-            //this.sensordataTableAdapter.Fill(this.dSsensor.sensordata);
-
+            //init datetime picker
+            dtpStart.Value = earliest_date;
             cn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
             cn.Open();
-            cm.CommandText = "select * from sensordata order by Id desc";
+            cm.CommandText = "select * from sensordata where datediff(day,'" + dtpStart.Value.ToString() + "',record_datetime)=0 order by record_datetime";
             cm.Connection = cn;
             da = new SqlDataAdapter(cm);
             da.Fill(dt);
             dataGridView1.DataSource = dt;
             timer1.Start();
+            this.MouseWheel += new MouseEventHandler(frmHistory_MouseWheel);
         }
+
+        
 
         
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (chkAutoRefresh.Checked)
             {
-                dt.Clear();
-                da.Fill(dt);
+                ProcessData();
+            }
+        }
+        
+        private void ProcessData()
+        {
+            if (LocktheDatatable)
+                return;
+            dt.Clear();
+            cm.CommandText = "select * from sensordata where datediff(day,'" + dtpStart.Value.ToString() + "',record_datetime)=0 order by record_datetime";
+            da.Fill(dt);
+            if (chkShowDiagram.Checked)
+            {
+                chkShowDiagram.Text = "显示表格";
+                dataGridView1.Hide();
+            }
+            else
+            {
+                chkShowDiagram.Text = "显示折线图";
+                dataGridView1.Show();
                 dataGridView1.DataSource = dt;
             }
-                
+            this.Refresh();
         }
 
         private void frmHistory_FormClosing(object sender, FormClosingEventArgs e)
@@ -54,16 +78,182 @@ namespace BD_Dashboard
             timer1.Stop();
         }
 
+        //delete all local records
         private void button1_Click(object sender, EventArgs e)
         {
             SqlCommand cmdelete = new SqlCommand();
             cmdelete.Connection = cn;
-
             cmdelete.CommandText = "delete from sensordata";
             cmdelete.ExecuteNonQuery();
             dt.Clear();
             da.Fill(dt);
             dataGridView1.DataSource = dt;
+        }
+
+        private int MouseDelta = 8;
+        void frmHistory_MouseWheel(object sender, MouseEventArgs e)
+        {
+            MouseDelta = MouseDelta - e.Delta / 120;
+            this.Refresh();
+            Console.WriteLine(MouseDelta.ToString());
+        }
+
+        private void chkShowDiagram_CheckedChanged(object sender, EventArgs e)
+        {
+            ProcessData();
+        }
+
+        public const int axis_x_0 = 20;
+        public const int axis_x_max = 20;
+        public const int axis_y_0 = 60;
+        public const int axis_y_max = 60;
+        public const int xfactor = 4;
+        public const int yscale = 20;
+        public const int yoffset = 200;
+
+        private bool LocktheDatatable = false;
+        DateTime FirstDateT = new DateTime();
+        private void frmHistory_Paint(object sender, PaintEventArgs e)
+        {
+            if (isResizing)
+                return;
+            Graphics g = e.Graphics;
+            //draw axis
+            //draw x
+            int Y_0 = this.Height - axis_y_0;
+            g.DrawLine(new Pen(Color.Black), new Point(axis_x_0, Y_0), new Point(this.Width-axis_x_max, Y_0));
+            //draw y
+            g.DrawLine(new Pen(Color.Black), new Point(axis_x_0, Y_0), new Point(axis_x_0, axis_y_max));
+            //draw 10C x
+            g.DrawLine(new Pen(Color.Black), new Point(axis_x_0, Y_0 - 10 * yscale+yoffset), new Point(this.Width - axis_x_max, Y_0 - 10 * yscale+yoffset));
+            g.DrawString("10C", new Font(FontFamily.GenericSansSerif, 8), new SolidBrush(Color.Black), new Point(0, Y_0 - 10 * yscale + yoffset));
+            //draw 20C x
+            g.DrawLine(new Pen(Color.Black), new Point(axis_x_0, Y_0 - 20 * yscale + yoffset), new Point(this.Width - axis_x_max, Y_0 - 20 * yscale + yoffset));
+            g.DrawString("20C", new Font(FontFamily.GenericSansSerif, 8), new SolidBrush(Color.Black), new Point(0, Y_0 - 20 * yscale + yoffset));
+            //draw 30C x
+            g.DrawLine(new Pen(Color.Black), new Point(axis_x_0, Y_0 - 30 * yscale + yoffset), new Point(this.Width - axis_x_max, Y_0 - 30 * yscale + yoffset));
+            g.DrawString("30C", new Font(FontFamily.GenericSansSerif, 8), new SolidBrush(Color.Black), new Point(0, Y_0 - 30 * yscale + yoffset));
+            
+            
+            if (dt.Rows.Count == 0)
+                return;
+            LocktheDatatable = true;
+            //try to draw temp2
+            DataRow[] drstemp2 = dt.Select("type='temp2'");
+            FirstDateT = DateTime.Parse(drstemp2[0]["record_datetime"].ToString());
+            //------------draw date labels--------------------
+            for(int i=1;i<=23;i++)
+            {
+                TimeSpan dts1 = DateTime.Parse(dtpStart.Value.Year.ToString()+"-"+dtpStart.Value.Month.ToString()+"-"+dtpStart.Value.Day.ToString()+" "+i.ToString()+":00") - FirstDateT;
+                int dx = Convert.ToInt32(dts1.TotalSeconds * xfactor);
+                g.DrawLine(new Pen(Color.Black), new Point(Convert.ToInt32((dx) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0), new Point(Convert.ToInt32((dx) / Math.Pow(2, MouseDelta)) + axis_x_0, axis_y_max));
+                g.DrawString(i.ToString()+":00", new Font(FontFamily.GenericSansSerif, 8), new SolidBrush(Color.Black), new Point(Convert.ToInt32((dx) / Math.Pow(2, MouseDelta)), Y_0));
+            }
+
+            //draw actual data
+            Point lastpoint = new Point(-1, -1);
+            Pen mypen = new Pen(Color.Red);
+            foreach (DataRow dr in drstemp2)
+            {
+                double value = (double)dr["value"];
+                int drawvalue = Convert.ToInt32(Math.Round(value * yscale - yoffset, 0));
+                string type = dr["type"].ToString();
+                DateTime record_datet = (DateTime)dr["record_datetime"];
+                //only assign the 1st time
+                TimeSpan ts1 = record_datet-FirstDateT;
+                int deltax = Convert.ToInt32(ts1.TotalSeconds*xfactor);
+                if (lastpoint.X == -1 && lastpoint.Y == -1)
+                {
+                    lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+                    g.DrawArc(mypen, new Rectangle(lastpoint.X-2, lastpoint.Y-2, 4, 4), 0, 360);
+                    continue;
+                }
+                
+                g.DrawLine(mypen, lastpoint,new Point(Convert.ToInt32((deltax)/Math.Pow(2,MouseDelta))+axis_x_0 , Y_0 - drawvalue));
+                g.DrawArc(mypen, new Rectangle(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0 - 2, Y_0 - 2 - drawvalue, 4, 4), 0, 360);
+                //Console.WriteLine("x:" + (deltax).ToString() + ",Y:" + (drawvalue).ToString());
+                //Console.Write("x1:" + (axis_x_0 + deltax).ToString() + ",y1:" + (Y_0 - drawvalue).ToString() + ",x2:" + (axis_x_0 + deltax + 1).ToString() + ",y2:" + (Y_0 - drawvalue).ToString());
+                //Console.WriteLine(FirstDateT.ToShortTimeString() + " " + record_datet.ToShortTimeString());
+                lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+            }
+            //try to draw temp3
+            DataRow[] drstemp3 = dt.Select("type='temp3'");
+            FirstDateT = DateTime.Parse(drstemp3[0]["record_datetime"].ToString());
+            lastpoint = new Point(-1, -1);
+            mypen = new Pen(Color.Purple);
+            foreach (DataRow dr in drstemp3)
+            {
+                double value = (double)dr["value"];
+                int drawvalue = Convert.ToInt32(Math.Round(value * yscale - yoffset, 0));
+                string type = dr["type"].ToString();
+                DateTime record_datet = (DateTime)dr["record_datetime"];
+                //only assign the 1st time
+                TimeSpan ts1 = record_datet - FirstDateT;
+                int deltax = Convert.ToInt32(ts1.TotalSeconds * xfactor);
+                if (lastpoint.X == -1 && lastpoint.Y == -1)
+                {
+                    lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+                    g.DrawArc(mypen, new Rectangle(lastpoint.X - 2, lastpoint.Y - 2, 4, 4), 0, 360);
+                    continue;
+                }
+
+                g.DrawLine(mypen, lastpoint, new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue));
+                g.DrawArc(mypen, new Rectangle(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0 - 2, Y_0 - 2 - drawvalue, 4, 4), 0, 360);
+                //Console.WriteLine("x:" + (deltax).ToString() + ",Y:" + (drawvalue).ToString());
+                //Console.Write("x1:" + (axis_x_0 + deltax).ToString() + ",y1:" + (Y_0 - drawvalue).ToString() + ",x2:" + (axis_x_0 + deltax + 1).ToString() + ",y2:" + (Y_0 - drawvalue).ToString());
+                //Console.WriteLine(FirstDateT.ToShortTimeString() + " " + record_datet.ToShortTimeString());
+                lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+            }
+            //try to draw temp4
+            DataRow[] drstemp4 = dt.Select("type='temp4'");
+            FirstDateT = DateTime.Parse(drstemp4[0]["record_datetime"].ToString());
+            lastpoint = new Point(-1, -1);
+            mypen = new Pen(Color.Green);
+            foreach (DataRow dr in drstemp4)
+            {
+                double value = (double)dr["value"];
+                int drawvalue = Convert.ToInt32(Math.Round(value * yscale-yoffset, 0));
+                string type = dr["type"].ToString();
+                DateTime record_datet = (DateTime)dr["record_datetime"];
+                //only assign the 1st time
+                TimeSpan ts1 = record_datet - FirstDateT;
+                int deltax = Convert.ToInt32(ts1.TotalSeconds * xfactor);
+                if (lastpoint.X == -1 && lastpoint.Y == -1)
+                {
+                    lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+                    g.DrawArc(mypen, new Rectangle(lastpoint.X - 2, lastpoint.Y - 2, 4, 4), 0, 360);
+                    continue;
+                }
+
+                g.DrawLine(mypen, lastpoint, new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue));
+                g.DrawArc(mypen, new Rectangle(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0 - 2, Y_0 - 2 - drawvalue, 4, 4), 0, 360);
+                //Console.WriteLine("x:" + (deltax).ToString() + ",Y:" + (drawvalue).ToString());
+                //Console.Write("x1:" + (axis_x_0 + deltax).ToString() + ",y1:" + (Y_0 - drawvalue).ToString() + ",x2:" + (axis_x_0 + deltax + 1).ToString() + ",y2:" + (Y_0 - drawvalue).ToString());
+                //Console.WriteLine(FirstDateT.ToShortTimeString() + " " + record_datet.ToShortTimeString());
+                lastpoint = new Point(Convert.ToInt32((deltax) / Math.Pow(2, MouseDelta)) + axis_x_0, Y_0 - drawvalue);
+            }
+
+            Console.WriteLine("end of cycle");
+            LocktheDatatable = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessData();
+        }
+
+        bool isResizing = false;
+        private void frmHistory_ResizeBegin(object sender, EventArgs e)
+        {
+            isResizing = true;
+            Console.WriteLine("resize begins");
+        }
+
+        private void frmHistory_ResizeEnd(object sender, EventArgs e)
+        {
+            isResizing = false;
+            ProcessData();
+            Console.WriteLine("resize ends");
         }
     }
 }
